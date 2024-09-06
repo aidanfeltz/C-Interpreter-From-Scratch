e53bc2a39f2ff76a24663de4d596ca626060528b
aidanfeltz <154996275+aidanfeltz@users.noreply.github.com>
2024-09-05 21:19:54 -0600

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,30 @@
+file_path_cinterpreter = input("input path of c code or the name of the file if in the directory of the file")
+utf_or_ascii_cinterpreter = input("input UTF8 for unicode or ASCII for ASCII")
+with open(str((file_path_cinterpreter)),"rb") as file:
+    c_binary_cinterpreter = file.read()
+    c_text_cinterpreter = c_binary_cinterpreter.decode(str(utf_or_ascii_cinterpreter))
+stdio_cinterpreter = False #will only support printf and scanf  
+stdlib_cinterpreter = False
+
+#preprocessing
+preprocessing_macros_cinterpreter = [] #will only support macros for now
+def 
+
+#to avoid if else hell
+
+def macro_cinterpreter:
+
+
+declarations ={
+    "#define":macro_cinterpreter(),
+    "int":integar_cinterpreter()
+    ""
+}
+case = 0
+for line_cinterpreter in c_text_cinterpreter:
+    words_cinterpreter = line_cinterpreter.split()
+    for word_cinterpreter in words_cinterpreter:
+        if case_cinterpreter = 0:
+        
+        
+        
